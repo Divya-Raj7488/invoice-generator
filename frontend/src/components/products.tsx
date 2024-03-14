4df672f4f3492fe80141f8553dd9fcba0b27a400
Divya Raj <divyaraj7612@gmail.com>
2024-03-14 15:39:28 +0530

    product addition and invoice generation successful

--- frontend/src/components/products.tsx
+++ frontend/src/components/products.tsx
@@ -15,6 +15,7 @@ const AddProduct: React.FC = () => {
   const [productRate, setProductRate] = useState("");
   const [DataColleted, setDataCollected] = useState<Product[]>([]);
   const [isPdfGenerated, setIsPdfGenerated] = useState(false);
+  const [genInvoice, setGenInvoice] = useState(false);
 
   const addMore = () => {
     const newProduct: Product = {
@@ -24,25 +25,30 @@ const AddProduct: React.FC = () => {
       total: parseInt(productRate) * parseInt(productQuantity),
       totalWithGST: parseInt(productRate) * parseInt(productQuantity) * 1.18,
     };
+    console.log(DataColleted);
     setDataCollected((prev: Product[]) => [...prev, newProduct]);
     setproductName("");
     setProductQuantity("");
     setProductRate("");
+    console.log(DataColleted);
+    return true;
+  };
+
+  const EnableDownload = async () => {
+    const updateData = addMore();
+    if (updateData) {
+      //enable download btn
+      setGenInvoice(true);
+    } else {
+      console.log("error occured while updating DataCollected state");
+    }
   };
   const generateInvoice = async () => {
-    const newProduct: Product = {
-      productName: productName,
-      productQuantity: productQuantity,
-      productRate: productRate,
-      total: parseInt(productRate) * parseInt(productQuantity),
-      totalWithGST: parseInt(productRate) * parseInt(productQuantity) * 1.18,
-    };
-    setDataCollected((prev: Product[]) => [...prev, newProduct]);
     try {
-      // console.log(DataColleted)
+      console.log(DataColleted);
       const response = await axios({
         method: "POST",
-        data: newProduct,
+        data: DataColleted,
         url: "http://localhost:3000/create-product",
         withCredentials: true,
       });
@@ -54,6 +60,7 @@ const AddProduct: React.FC = () => {
       console.log(error);
     }
   };
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-80 h-96 flex flex-col justify-center items-center rounded-3xl gap-3 loginForm">
@@ -82,7 +89,7 @@ const AddProduct: React.FC = () => {
         <div>
           <button
             className="w-28 h-10 bg-green-400 rounded-lg text-lg text-slate-800 font-medium"
-            onClick={generateInvoice}
+            onClick={EnableDownload}
           >
             Next
           </button>{" "}
@@ -94,6 +101,16 @@ const AddProduct: React.FC = () => {
           </button>
         </div>
         <div>
+          {genInvoice && (
+            <button
+              className="w-32 h-10 rounded-lg bg-yellow-500 text-lg text-slate-800 font-medium"
+              onClick={generateInvoice}
+            >
+              Generate pdf
+            </button>
+          )}
+        </div>
+        <div>
           {/* {DataColleted.length > 0 && DataColleted.map(() =>{})} */}
         </div>
       </div>
@@ -101,11 +118,8 @@ const AddProduct: React.FC = () => {
         <div className="w-80 h-96 flex flex-col justify-center items-center rounded-3xl gap-3 loginForm">
           {/* show generated pdf here */}
           <div></div>
-
         </div>
       )}
-
-      {/* <h1>hello</h1> */}
     </div>
   );
 };
